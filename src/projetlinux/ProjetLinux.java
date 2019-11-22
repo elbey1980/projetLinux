aced842f46eef469dd2f59d3f1aa831171e4d338
usager <usager@10.0.2.15>
2019-11-22 15:43:16 -0500

    version 4

--- src/projetlinux/ProjetLinux.java
+++ src/projetlinux/ProjetLinux.java
@@ -17,7 +17,7 @@ public class ProjetLinux {
     public static void main(String[] args) {
        System.out.println("Bonjour "+args[1]+" , "+args[0]);
         System.out.println("Au revoir");
-        System.out.println("test push");
+        System.out.println("test push 2");
     }
     
 }
